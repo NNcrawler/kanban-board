6ce97efe1d719dd0b174a6ef693ca9568c411c96
NNcrawler <rasyid.xyz@gmail.com>
2017-11-04 15:46:50 +0700

    added backlog, new task and layout

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <board-container/>
+  </div>
+</template>
+
+<script>
+import boardContainer from './components/board-container';
+
+export default {
+  name: 'app',
+  created() {
+  },
+  components: {
+    'board-container': boardContainer,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/board-container.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <b-container>
+    <b-row><b-col><new-task/></b-col></b-row>
+    <b-row>
+      <b-col>
+        <card :tasks="backlog" title="Backlog"/>
+      </b-col>
+      <b-col>
+        <card :tasks="todos" title="Todo"/>
+      </b-col>
+      <b-col>
+        <card title="Doing"/>
+      </b-col>
+      <b-col>
+        <card title="Done"/>
+      </b-col>
+    </b-row>
+    <!-- <mymodal> -->
+  </b-container>
+</template>
+
+<script>
+import list from './card-list';
+import newTask from './new-task';
+
+export default {
+  components: {
+    card: list,
+    'new-task': newTask,
+  },
+  firebase() {
+    return {
+      todos: this.$db.ref('/todos'),
+      backlog: this.$db.ref('/backlog'),
+    };
+  },
+};
+</script>
+
+<style lang="css">
+</style>
