200e136f744d5ac3d2221f9bee0b25a72a005aba
NNcrawler <rasyid.xyz@gmail.com>
2017-11-04 17:20:47 +0700

    added delete task

--- src/components/card-list.vue
+++ src/components/card-list.vue
@@ -1,15 +1,29 @@
 <template lang="html">
-  <div v-b-modal.modal1 class="">
+  <div class="">
     <b-card v-bind:title="title">
-      <b-card v-if="tasks && tasks.length>0" v-for="task in tasks" :bg-variant="cardColors"
+      <b-card @click='showModal(task)' v-for="task in tasks" :bg-variant="cardColors"
              :header="task.title"
              class="task mb-2"
              text-variant="white">
          <p class="card-text">{{task.description}}</p>
          <small class="text-light" slot="footer">Assigned to: {{task.assignedTo}} <b-badge pill variant="success">{{task.point}}</b-badge></small>
      </b-card>
-     <b-alert v-else show variant="info">Nothing here yet.</b-alert>
+     <b-alert v-if="!tasks || tasks.length==0" show variant="info">Nothing here yet.</b-alert>
     </b-card>
+
+    <b-modal ref="detail-task" id="detail-task">
+      <div slot="modal-title">
+        <h3>{{taskDetails.title}}</h3>
+        <div class="task-status"><small>status: {{taskDetails.status}}</small></b-badge></div>
+        <div class="task-status"><small><b-badge pill variant="success">Point: {{taskDetails.point}}</b-badge></small></div>
+      </div>
+      <p> {{taskDetails.description}}</p>
+      <div slot="modal-footer">
+        <b-button variant="link" v-on:click="deleteTask" type="button" name="button">Delete</b-button>
+        <b-button v-if="beforeAvailable" variant="primary" type="button" name="button">Move Before</b-button>
+        <b-button v-if="nextAvailable" variant="primary" type="button" name="button">Move Next</b-button>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -17,18 +31,70 @@
 export default {
   data() {
     return {
+      taskDetails: {
+        title: '',
+        description: '',
+        point: '',
+        status: '',
+        key: '',
+      },
     };
   },
   props: ['title', 'tasks'],
   computed: {
     cardColors() {
-      return 'danger';
+      if (this.title === 'Backlog') {
+        return 'danger';
+      } else if (this.title === 'Todo') {
+        return 'warning';
+      } else if (this.title === 'Doing') {
+        return 'info';
+      } else if (this.title === 'Done') {
+        return 'success';
+      }
+      return '';
+    },
+    beforeAvailable() {
+      if (this.title === 'Backlog') {
+        return false;
+      }
+      return true;
+    },
+    nextAvailable() {
+      if (this.title === 'Done') {
+        return false;
+      }
+      return true;
+    },
+  },
+  methods: {
+    showModal(task) {
+      this.taskDetails.title = task.title;
+      this.taskDetails.description = task.description;
+      this.taskDetails.point = task.point;
+      this.taskDetails.key = task['.key'];
+      if (this.title === 'Backlog') {
+        this.taskDetails.status = 'Backlog';
+      } else if (this.title === 'Todo') {
+        this.taskDetails.status = 'Todo';
+      }
+      this.$refs['detail-task'].show();
+    },
+    deleteTask() {
+      if (this.title === 'Backlog') {
+        this.$db.ref(`/backlog/${this.taskDetails.key}`).remove();
+      }
     },
   },
 };
 </script>
 
 <style lang="css">
+.task-status {
+  margin: 0 !important;
+  padding: 0 !important;
+  text-align: left;
+}
 .task:hover {
   cursor: pointer;
 	-webkit-animation: shadow-drop-2-center 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
